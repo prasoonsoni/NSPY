fbe67791101db4a3c529de9c5d715e9d8f966c91
Shantanu Digambar Dahitule <79780881+shantanu-dahitule@users.noreply.github.com>
2021-08-29 01:13:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,39 +13,8 @@ def flipkart():
     open('output.jl', 'w').close()
 
     if product_name:
-        p = subprocess.check_output(
-            'scrapy runspider flipkart.py -a category="{}" -o output.jl'.format(product_name), 
-            shell=True)
-        
         # print(e.output)
         data = []
-        with open('output.jl') as f:
-            for line in f:
-                data.append(json.loads(line))
-        return jsonify(data)
-    return "Querry param not passed"
-
-
-@app.route('/amazon', methods = ['GET'])
-def amazon():
-    product_name =  request.args['q']
-
-    open('output.jl', 'w').close()
-
-    if product_name:
-        p = subprocess.check_output(
-            'scrapy runspider amazon.py -a category="{}" -o output.jl'.format(product_name), 
-            shell=True)
-        
-        # print(e.output)
-        data = []
-        with open('output.jl') as f:
-            for line in f:
-                data.append(json.loads(line))
-        return jsonify(data)
-    return "Querry param not passed"
-
-"""
  # flipkart
         p = subprocess.check_output(
             'scrapy runspider flipkart.py -a category="{}" -o output.jl'.format(product_name), 
@@ -64,7 +33,7 @@ def amazon():
 
 
         return jsonify(sorted(data, key = lambda i: i['product_price']))
-    return "Querry param not passed""""
+    return "Querry param not passed"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
