ed4fd6d8ebdd20e50db5810e745810ae8df4a78a
Prasoon Soni <prasoonsoni03@gmail.com>
2021-08-28 23:41:44 +0530

    Update

--- app.py
+++ app.py
@@ -6,54 +6,36 @@ import json
 
 app = Flask(__name__)
 
-@app.route('/data', methods = ['GET'])
+@app.route('/flipkart', methods = ['GET'])
 def flipkart():
     product_name =  request.args['q']
-
     open('output.jl', 'w').close()
-
     if product_name:
-        # p = subprocess.check_output(
-        #     'scrapy runspider flipkart.py -a category="{}" -o output.jl'.format(product_name), 
-        #     shell=True)
-        
-        # # print(e.output)
-        data = []
-        # with open('output.jl') as f:
-        #     for line in f:
-        #         data.append(json.loads(line))
         p = subprocess.check_output(
-            'scrapy runspider amazon.py -a category="{}" -o output.jl'.format(product_name), 
+            'scrapy runspider flipkart.py -a category="{}" -o output.jl'.format(product_name), 
             shell=True)
-        
-        # print(e.output)
-        #data = []
+        data = []
         with open('output.jl') as f:
             for line in f:
-                data.append(json.loads(line))                
+                data.append(json.loads(line))             
         return jsonify(sorted(data, key = lambda i: i['product_price']))
     return "Querry param not passed"
 
-"""
+
 @app.route('/amazon', methods = ['GET'])
 def amazon():
     product_name =  request.args['q']
-
     open('output.jl', 'w').close()
-
     if product_name:
         p = subprocess.check_output(
             'scrapy runspider amazon.py -a category="{}" -o output.jl'.format(product_name), 
             shell=True)
-        
-        # print(e.output)
         data = []
         with open('output.jl') as f:
             for line in f:
                 data.append(json.loads(line))
         return jsonify(data)
     return "Querry param not passed"
-"""
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
