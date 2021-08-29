ee76fda6e53036b3d98e8fb33f569b4d81764a92
Prasoon Soni <prasoonsoni03@gmail.com>
2021-08-29 14:55:24 +0530

    Update

--- flipkart.py
+++ flipkart.py
@@ -11,26 +11,28 @@ class FlipkartSpider(scrapy.Spider):
         names = response.xpath("//div[@class='_4rR01T']/text()").getall()
         prices = response.xpath("//div[@class='_30jeq3 _1_WHN1']/text()").getall()
         imgs = response.xpath("//img[@class='_396cs4 _3exPp9']/@src").getall()
-        for i in range(len(names)):
-            yield {
-                "shopping_site":"flipkart", 
-                "product_name":names[i], 
-                "product_price":int((prices[i].replace("\u20b9", "")).replace(",","")), 
-                "product_image":imgs[i]
-            }
+        for i in range(min(len(names),len(prices),len(imgs))):
+            if names[i] and prices[i] and imgs[i]:
+                yield {
+                    "shopping_site":"flipkart", 
+                    "product_name":names[i], 
+                    "product_price":int((prices[i].replace("\u20b9", "")).replace(",","")), 
+                    "product_image":imgs[i]
+                }
 
         names = response.xpath("//a[@class='s1Q9rs']/text()").getall()
         prices = response.xpath("//div[@class='_30jeq3']/text()").getall()
         imgs = response.xpath("//img[@class='_396cs4 _3exPp9']/@src").getall()
-        print(names)
-        print(imgs)
-        print(prices)
+        print(len(names))
+        print(len(imgs))
+        print(len(prices))
         
-        for i in range(len(names)):
-            yield {
-                "shopping_site":"flipkart", 
-                "product_name":names[i], 
-                "product_price":int((prices[i].replace("\u20b9", "")).replace(",","")), 
-                "product_image":imgs[i]
-            }
+        for i in range(min(len(names),len(prices),len(imgs))):
+            if names[i] and prices[i] and imgs[i]:
+                yield {
+                    "shopping_site":"flipkart", 
+                    "product_name":names[i], 
+                    "product_price":int((prices[i].replace("\u20b9", "")).replace(",","")), 
+                    "product_image":imgs[i]
+                }
 
