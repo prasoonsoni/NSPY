3d19803c0637392752352d310b38a360e2201921
Shantanu Digambar Dahitule <79780881+shantanu-dahitule@users.noreply.github.com>
2021-08-29 14:36:35 +0530

    Update flipkart.py

--- flipkart.py
+++ flipkart.py
@@ -10,14 +10,17 @@ class FlipkartSpider(scrapy.Spider):
     def parse(self, response):
         names = response.xpath("//div[@class='_4rR01T']/text()").getall()
         prices = response.xpath("//div[@class='_30jeq3 _1_WHN1']/text()").getall()
+            
         imgs = response.xpath("//img[@class='_396cs4 _3exPp9']/@src").getall()
         for i in range(len(names)):
-            yield {
-                "shopping_site":"flipkart", 
-                "product_name":names[i], 
-                "product_price":int((prices[i].replace("\u20b9", "")).replace(",","")), 
-                "product_image":imgs[i]
-            }
+             if names[i] and prices[i] and imgs[i]:
+                yield {
+                    "shopping_site":"flipkart", 
+                    "product_name":names[i], 
+                    "product_price":int((prices[i].replace("\u20b9", "")).replace(",","")), 
+                    "product_image":imgs[i]
+                }
+
 
         names = response.xpath("//a[@class='s1Q9rs']/text()").getall()
         prices = response.xpath("//div[@class='_30jeq3']/text()").getall()
@@ -27,10 +30,11 @@ class FlipkartSpider(scrapy.Spider):
         print(prices)
         
         for i in range(len(names)):
-            yield {
-                "shopping_site":"flipkart", 
-                "product_name":names[i], 
-                "product_price":int((prices[i].replace("\u20b9", "")).replace(",","")), 
-                "product_image":imgs[i]
-            }
+            if names[i] and prices[i] and imgs[i]:
+                yield {
+                    "shopping_site":"flipkart", 
+                    "product_name":names[i], 
+                    "product_price":int((prices[i].replace("\u20b9", "")).replace(",","")), 
+                    "product_image":imgs[i]
+                }
 
