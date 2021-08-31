74a51378ff8bed4320c36638fa09c6af33a5cce2
Shantanu Digambar Dahitule <79780881+shantanu-dahitule@users.noreply.github.com>
2021-08-31 18:08:05 +0530

    Update amazon.py

--- amazon.py
+++ amazon.py
@@ -1,7 +1,6 @@
 import scrapy
 from scrapy import settings
-set = settings()
-set.USER_AGENT = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36"
+settings.USER_AGENT = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36"
 class AmazonSpider(scrapy.Spider):
     name = 'amazon'
     allowed_domains = ['amazon.com']
